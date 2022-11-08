99d2b9b6bc35d37f2e7e9806554f0dc96824f853
nehasonavane024 <nehasonavane1999@gmail.com>
2022-11-08 13:37:31 +0530

    first commit

---
+++ src/components/home/HomePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import QuickSearch from "./QuickSearch";
+import Wallpaper from "./Wallpaper";
+
+function HomePage() {
+  return (
+    <>
+      <Wallpaper />
+      <QuickSearch />
+    </>
+  );
+}
+
+export default HomePage;
---
+++ src/components/search/SearchPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Header from "../Header";
+import SearchPageResult from "./SearchPageResult";
+
+function SearchPage() {
+  return (
+    <>
+      <Header color="bg-danger" />
+      <SearchPageResult />
+    </>
+  );
+}
+
+export default SearchPage;
